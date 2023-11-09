04219f81107c079877d9c5b0338f10a1b2cc6fd9
bkubick <bjk2158@columbia.edu>
2023-11-09 15:41:41 -0600

    Move bubble sort docstring

--- cpp-dsa/algorithms/sort.cpp
+++ cpp-dsa/algorithms/sort.cpp
@@ -3,13 +3,6 @@
 #include "sort.hpp"
 
 
-/**
- * Bubble sort algorithm that sorts an array of integers in ascending order,
- * in place.
- * 
- * @param arr[] array to be sorted.
- * @param size size of the array.
-*/
 void bubbleSort(int arr[], int size)
 {
     
--- cpp-dsa/algorithms/sort.hpp
+++ cpp-dsa/algorithms/sort.hpp
@@ -1,1 +1,8 @@
+/**
+ * Bubble sort algorithm that sorts an array of integers in ascending order,
+ * in place.
+ * 
+ * @param arr[] array to be sorted.
+ * @param size size of the array.
+*/
 void bubbleSort(int arr[], int size);
